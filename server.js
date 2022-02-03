c7d6f6e7fbbf11ab76b47cdb9cad058ad31b03ce
choonsikkang <98009549+choonsikkang@users.noreply.github.com>
2022-02-03 11:39:11 +0900

    API add, 응답 JSON add

--- server.js
+++ server.js
@@ -12,23 +12,253 @@ app.use(express.urlencoded({
 
 app.use('/api', apiRouter);
 
-apiRouter.post('/sayHello', (req, res) => {
-    const responseBody = {
-      version: "2.0",
-      template: {
-        outputs: [
+/* TODO: 맨 처음 채널을 추가하고 웰컴 블록이 등장 후, 아무 말이나 입력했을 때, 
+사용자가 어떤 발화를 사용할 수 있는지 도와주는 폴백블록을 제공해주려고 만든다.
+*/
+apiRouter.post('/help', (req, res) => {
+  console.log(req.body);
+
+  const responseBody = {
+    "version": "2.0",
+      "template": {
+        "outputs": [
           {
-            simpleText: {
-              text: "hello I'm Ryan"
+            "simpleText": {
+              "text": "아래 단어들을 보고, 원하는 단어를 클릭해주세요🤗"
             }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "주문방법",
+            "action": "message",
+            "label": "주문방법"
+          },
+          {
+            "messageText": "메뉴판",
+            "action": "message",
+            "label": "메뉴판"
+          },
+          {
+            "messageText": "주문할게요",
+            "action": "message",
+            "label": "주문할게요"
+          },
+          {
+            "messageText": "안녕",
+            "action": "message",
+            "label": "안녕"
+          },
+          {
+            "messageText": "안녕안녕",
+            "action": "message",
+            "label": "안녕안녕"
           }
         ]
       }
-    };
-  
+    }
+  res.status(200).send(responseBody);
+});
+
+apiRouter.post('/how', (req, res) => {
+  console.log(req.body);
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          basicCard: {
+            title: "주문할게요",
+            description: "메뉴를 말하면 바로 주문이 시작되어요",
+            thumbnail: {
+              imageUrl: "https://t2.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1oU7/image/ZttOOUGN88HuP5FXYMBzrWowP_c.png"
+            },
+            buttons: [
+              {
+                action: "message",
+                label: "주문하는 방법",
+                messageText: "주문하는 방법"
+              },
+              {
+                action:  "webLink",
+                label: "구경하기",
+                webLinkUrl: "https://e.kakao.com/t/hello-ryan"
+              }
+            ]
+          }
+        }
+        
+      ]
+    }
+  }
+
     res.status(200).send(responseBody);
   });
-  
+
+// TODO Anchors Test
+apiRouter.post('/order', (req, res) => {
+  const responseBody = {
+    version: "2.0",
+    template: {
+        outputs: [
+            {
+                simpleText: {
+                    // text: "아직 주문이 안돼요. 개발중입니다.😭"
+                    text: '++장바구니++\n------------------------------------\n아이스 아메리카노 > 1개'
+                }
+            }
+        ]
+    }
+}
+  res.status(200).send(responseBody);
+});
+
+// TODO Anchors Test
+apiRouter.post('/menu', (req, res) => {
+  console.log(req.body);
+
+  const responseBody = {
+    "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "simpleText": {
+              "text": "안녕 난 애기 메뉴판!😁"
+            }
+          }
+        ],
+        "quickReplies": [
+          {
+            "messageText": "커피",
+            "action": "message",
+            "label": "커피"
+          },
+          {
+            "messageText": "음료",
+            "action": "message",
+            "label": "음료"
+          },
+          {
+            "messageText": "스페셜",
+            "action": "message",
+            "label": "스페셜"
+          },
+          {
+            "messageText": "차",
+            "action": "message",
+            "label": "차"
+          }
+        ]
+      }
+    }
+  res.status(200).send(responseBody);
+});
+
+apiRouter.post('/coffee', (req, res) => {
+    const responseBody = {
+      "version": "2.0",
+      "template": {
+        "outputs": [
+      {
+        "carousel": {
+          "type": "listCard",
+          "items": [
+            {
+              "header": {
+                "title": "샌드위치"
+              },
+              "items": [
+                {
+                  "title": "햄치즈",
+                  "description": "4,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_01.jpg"
+                },
+                {
+                  "title": "베이컨 아보카도",
+                  "description": "5,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_02.jpg"
+                },
+                {
+                  "title": "에그 포테이토",
+                  "description": "5,300원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_03.jpg"
+                },
+                {
+                  "title": "갈릭 베이컨 토마토",
+                  "description": "5,800원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_04.jpg"
+                }
+              ],
+              "buttons": [
+                {
+                  "label": "더보기",
+                  "action": "message",
+                  "messageText" : "샌드위치 더보기"
+                }
+              ]
+            },
+            {
+              "header": {
+                "title": "커피"
+              },
+              "items": [
+                {
+                  "title": "아메리카노(ice, hot 가격동일)",
+                  "description": "2,000원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_05.jpg"
+                },
+                {
+                  "title": "카페라떼",
+                  "description": "3,000원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_06.jpg"
+                },
+                {
+                  "title": "카페모카",
+                  "description": "3,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_07.jpg"
+                },
+                {
+                  "title": "바닐라 라떼",
+                  "description": "3,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_08.jpg"
+                }
+              ],
+              "buttons": [
+                {
+                  "label": "더보기",
+                  "action": "message",
+                  "messageText" : "커피 더보기"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    ],
+    "quickReplies": [
+      {
+        "messageText": "인기 메뉴",
+        "action": "message",
+        "label": "인기 메뉴"
+      },
+      {
+        "messageText": "최근 주문",
+        "action": "message",
+        "label": "최근 주문"
+      },
+      {
+        "messageText": "장바구니",
+        "action": "message",
+        "label": "장바구니"
+      }
+    ]
+  }
+}
+  res.status(200).send(responseBody);
+});
+
+
 apiRouter.post('/showHello', (req, res) => {
     console.log(req.body);
   
@@ -48,6 +278,8 @@ apiRouter.post('/showHello', (req, res) => {
   
     res.status(200).send(responseBody);
   });
+
+
 
 app.listen(port, () => {
   console.log(`Example skill server listening on ${port}!`);
