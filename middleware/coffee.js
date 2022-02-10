b85dde830354a13d603757235334efa2e77b165d
choonsikkang <98009549+choonsikkang@users.noreply.github.com>
2022-02-10 18:16:01 +0900

    장바구니 입력 값 수정, 예외 처리 개발 중

--- middleware/coffee.js
+++ middleware/coffee.js
@@ -5,46 +5,65 @@ exports.postCoffee = async (req, res, next) => {
     const kakao = res.locals.kakao;
 
     console.log(kakao.action);
+    console.log(req.body);
+    console.log(req.body.action.detailParams.coffee_name.value);
     console.log("kakao log==================================");
+    try {
+        let box = await basket
+            .create({
+                name: req.body.action.detailParams.coffee_name.value,
+                amount: req.body.action.detailParams.CupCount.origin
+            })
+            .catch((err) => {
+                console.error(err);
+                // console.log(err.name);
+                // console.log(err.message);
+                // console.log(err.stack);
+                if (err instanceof SequelizeUniqueConstraintError) {
+                    console.log(e.name + ", " + e.message);
+                }
 
-    let box = await basket
-        .create({
-            name: req.body.action.detailParams.coffee_name.value, // 사용자의 발화를 디비에 저장하는 것이 옳은것인지?
-            amount: req.body.action.detailParams.CupCount.origin
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-
-    if (!box) {
-        return res.status(500);
-    }
-
-    if (kakao.name && kakao.amount) {
-        const result = await basket.count({
-            where: {
-                name: kakao.name
-            }
-        });
-        console.log("--------------------Count :", result);
-    }
-
-    let menu = await basket
-        .findAll({
-            where: {},
-            // attrbutes: [[sequelize.fn("COUNT", sequelize.col("amount"))]],
-            name: kakao.name,
-            amount: kakao.amount
-        })
-        .then((res) => {
-            console.log("조회 성공: ", res);
-            return res.map((m) => {
-                return m.dataValues;
+                if (err instanceof ER_DUP_ENTRY) {
+                    console.log(e.name + ", " + e.message);
+                }
             });
-        });
 
-    res.locals.menu = menu;
+        if (!box) {
+            return res.status(500);
+        }
 
-    console.log(`Coffee :${inspect(menu)}`);
-    next();
+        // if (kakao.name && kakao.amount) {
+        //     const result = await basket.count({
+        //         where: {
+        //             name: kakao.name
+        //         }
+        //     });
+        //     console.log("--------------------Count :", result);
+        // }
+
+        let menu = await basket
+            .findAll({
+                attributes: ["name", "amount"],
+                where: {}
+                // name: kakao.name,
+                // amount: kakao.amount
+            })
+            .then((res) => {
+                console.log("조회 성공: ", res);
+                return res.map((m) => {
+                    return m.dataValues;
+                });
+            });
+
+        res.locals.menu = menu;
+
+        console.log(`Coffee :${inspect(menu)}`);
+        next();
+    } catch (err) {
+        /* NOTE ER_DUP_ENTRY (unique key)에 대한 에러핸들링 구현해보기 */
+
+        // 에러를 발생시킨다고 한다.
+        throw new Error("Some unexpected error, may also be thrown by a library or the runtime.");
+        // console.error(err);
+    }
 };
