8e3791fc5bc5c3db0fbe9c24285ff5270201bc42
choonsikkang <98009549+choonsikkang@users.noreply.github.com>
2022-02-15 18:08:26 +0900

    개발 연습 중

--- main/index.js
+++ main/index.js
@@ -8,108 +8,96 @@ const { deleteCoffee } = require("./routes/delete");
 require("dotenv").config();
 
 /* TODO 앞으로 만들고 싶은 카톡 챗봇의 방향 및 계획
-    1. 데이터베이스 관계를 다시 정립, user_id를 얻어오는 방법, coffee.js에 create할 때, product_id를 추가해주는 것
-    2. 하나의 API로 분기하기 계획: route폴더로 coffee.js(주문하기 기능(create), 장바구니 기능(find)), update.js(주문한 내용을 변경하는 기능), delete.js(주문한 내용을 싹 삭제하는 기능) 
-       등을 route 폴더로 이동, 즉, 어떤 api가 움직이면 실행되는 함수를 route폴더안에 이동
-    ```
-       const block = {
-        "block_id": function a() {}
-        "block_id": function b() {}
+    만들고 싶은 기능 MEMO
+*/
+
+/* NOTE 만들기 전에 block_id, intent_id로 들어오면, 어떠한 함수를 호출하려는 객체를 하나 선언해야한다.*/
+const BLOCK = fs
+    .readdirSync(`${__dirname}/routes`)
+    .filter((name) => name !== "index.js")
+    .map((name) => [name.split(".")[0], require(`${__dirname}/routes/${name}`)])
+    .reduce((obj, [filename, func]) => ({ ...obj, [filename]: func }), {});
+// line 19: obj라는 객체와 [filename, func]이라는 배열을 인자로 받고 obj는 구조분해할당으로 값을 펼치고, 배열안에 filename은 key, func은 value로 받고 새로운 객체에 담아 리턴한다.
+
+const { route } = require("../assets/intent");
+
+// (blockId, [title, sub])
+// const blockIdMap = Object.entries(route).reduce((blockIdMap, [title, block]) => {
+//     return Object.entries(block).reduce((blockIdMap, [sub, blockId]) => blockIdMap.set(blockId, [title, sub]), blockIdMap);
+// }, new Map());
+
+/* NOTE 하나의 API로 여러개의 발화들을 라우팅 */
+router.post("/", isKakao, postCoffee, deleteCoffee, updateCoffee, async (req, res) => {
+    // isKakao, postCoffee, updateCoffee, deleteCoffee,
+    // TypeError: Cannot read property 'value' of undefined
+    // 그니까 주문하기 기능에서 입력값이 없어서 에러가 나는 것 같고, 문법 오류도 앞에 값이 undefined라서 나는 것이다?
+
+    // console.log(req.body);
+    // console.log(BLOCK);
+    // console.log(blockIdMap);
+    // console.log(route);
+    // console.log("Hello world");
+
+    // const intentId = res.locals.kakao.intentId;
+    // console.log(blockIdMap.set(intentId));
+    // console.log(res.locals.route);
+    // let [title, sub] = blockIdMap.get(intentId) || [res.locals.menu.route.title, res.locals.menu.route.sub];
+    // console.log(blockIdMap.get(intentId));
+    // console.log([title, sub]);
+
+    /* NOTE for문으로 route의 값을 { route[i] : i } 으로 만듬 */
+    let object = {};
+    let intentId = "";
+    for (let i in route) {
+        object[route[i]] = i;
+        intentId += `${route[i]}: ${i}` + "\n";
+    }
+    // console.log(intentId);
+    console.log(object);
+
+    // object의 key, value 값 가져오기
+    let array = [];
+    for (let [key, value] of Object.entries(object)) {
+        console.log(`${key}, ${value}`);
+        array.push(key, value);
     }
 
-    const func = block[req.body.userRequest.block.id];
-    func() // 이 친구는 블록id에 대한 함수 값들을 출력하게 됨
-    ````
-*/
-const Block = require("../assets/intent");
+    /**
+     * NOTE 함수를 가져오는 객체를 하나 더 만들어야 해😭
+     * ex) 
+     * {
+        'postCoffee(실제 함수명)': postCoffee,
+        'updateCoffee(실제 함수명)': updateCoffee
+        }
+     */
 
-console.log(Block);
+    let [title, sub] = array;
+    const block = BLOCK && [title] && [sub];
+    console.log(block);
+    // // BLOCK?.[title]?.[sub];
+
+    // // title, sub를 통해 routes에서 함수 추출(???)
+    if (title && sub) {
+        console.log(`Title: ${title}, Sub: ${sub}`);
+    }
+
+    // // 블록
+    // if (block) {
+    //     await block(req, res);
+    // } else {
+    //     const turingMsg = await sendTuringMsg(req.body);
+    //     res.json(turingMsg);
+    // }
+});
 
 /* NOTE 원하는 커피를 주문하고, 장바구니에 담은 것을 보여주는 주문 기능 */
-router.post("/order", isKakao, postCoffee, async (req, res) => {
-    let arr = res.locals.menu;
+router.post("/order", isKakao, postCoffee, async (req, res) => {});
 
-    console.log("index log----------------------------------------------");
-    console.log(req.body);
-    console.log(arr); // DB에 있는 값들
-    // console.log(res.locals.menu.name);
+/* NOTE 주문한 커피를 변경하고, 장바구니에 변경된 물품들을 보여주는 주문 기능 */
+router.post("/modify", isKakao, updateCoffee, async (req, res) => {});
 
-    /* NOTE fs 모듈 연습 => 에러, Error: EISDIR: illegal operation on a directory, read */
-    // let BLOCK = fs.readFileSync("/home/kangjuhyeon/Kakao_Chatbot/assets", "utf-8", function (err, data) {
-    //     if (err) {
-    //         throw err;
-    //     } else {
-    //         console.log(data);
-    //     }
-    // });
-    // console.log(BLOCK);
-
-    /* NOTE 객체 값을 보여주기 위한 로직을 만들어야 된다. */
-    let text = "";
-    for (category in arr) {
-        var obj = arr[category];
-        text += obj.name + " " + ">" + " " + obj.amount + "\n";
-    }
-
-    const responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": `++장바구니++\n-----------------------------\n${text}개`
-                    }
-                },
-                {
-                    "simpleText": {
-                        "text": `합계 - 4500원`
-                    }
-                }
-            ],
-            "quickReplies": [
-                {
-                    "action": "block",
-                    "label": "주문 끝",
-                    "blockId": process.env.confirm_blockId
-                },
-                {
-                    "action": "block",
-                    "label": "주문 수정하기",
-                    "blockId": process.env.modify_blockId
-                }
-            ]
-        }
-    };
-    res.status(200).send(responseBody);
-});
-
-router.post("/modify", isKakao, updateCoffee, async (req, res) => {
-    let arr = res.locals.menu;
-
-    console.log(arr);
-
-    /* NOTE 객체 값을 보여주기 위한 로직을 만들어야 된다. */
-    let text = "";
-    for (category in arr) {
-        var obj = arr[category];
-        console.log(obj);
-        text += obj.name + " " + ">" + " " + obj.amount + "\n";
-    }
-
-    const responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": `++장바구니++\n-----------------------------\n${text}`
-                    }
-                }
-            ]
-        }
-    };
-    res.status(200).send(responseBody);
-});
+/* NOTE 장바구니에 있는 물품들을 전부 삭제 */
+router.post("/order_cancel", isKakao, deleteCoffee, async (req, res) => {});
 
 /* NOTE 주문하기 기능이 끝나고 주문을 더 이상 하지 않으면 주문 끝이라고 확인시키는 버튼 */
 router.post("/confirm", async (req, res) => {
@@ -141,41 +129,6 @@ router.post("/confirm", async (req, res) => {
         }
     };
     res.status(200).send(responseBody);
-});
-
-router.post("/order_cancel", deleteCoffee, async (req, res) => {
-    console.log(req.body);
-
-    const responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": `++장바구니++\n-----------------------------\n 주문내역이 없습니다. \n`
-                    }
-                }
-            ]
-        }
-    };
-    res.status(200).send(responseBody);
-});
-
-router.post("/total", async (req, res) => {
-    console.log(req.body);
-
-    const responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": `합계 - 4500원`
-                    }
-                }
-            ]
-        }
-    };
 });
 
 /* NOTE 카페의 메뉴 카테고리를 볼 수 있는 기능 
