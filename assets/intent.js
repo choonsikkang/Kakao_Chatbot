90fcbbc707aee0188e9e70b6cfa95a16923a07eb
choonsikkang <98009549+choonsikkang@users.noreply.github.com>
2022-02-17 17:07:36 +0900

    key, value 수정

--- assets/intent.js
+++ assets/intent.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
     "route": {
-        "order": process.env.order_blockId,
-        "modify": process.env.modify_blockId,
-        "order_cancel": process.env.ordercancel_blockId,
-        "menu": process.env.menu_blockId,
-        "confirm": process.env.confirm_blockId
+        "61f35921d10757597fffbf8d": "postCoffee",
+        "6205d90cca92880f0b4e5c6b": "updateCoffee",
+        "6204d2a5ba2e0d2c11795f23": "deleteCoffee",
+        "61f26293ca92880f0b4e24af": "menu",
+        "6203772fba2e0d2c1179579b": "confirm"
     }
 };
